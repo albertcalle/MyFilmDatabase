b3265a882d1e59d2a5b999134625d3e169490c1b
albertcalle <albert.calle.val@gmail.com>
2017-01-07 12:52:47 +0100

    first commit

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/Cerca.java
@@ -0,0 +1,17 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cerca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cerca);
+    }
+}
